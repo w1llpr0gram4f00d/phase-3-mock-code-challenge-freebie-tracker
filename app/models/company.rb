9db5d188720346bd2627ba6cb3f4f8122d4fe5c7
Marc Cavazos <marcavazos19@icloud.com>
2022-03-02 21:57:02 -0600

    commit tree 254fb6c74c87194ad722eb038107c80b0bd576c7

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie dev, item_name, value
+        self.freebies.create(dev_id: dev.id, company_id: self.id, item_name: item_name, value: value)
+    end
+
+    def self.oldest_company
+        self.all.minimum(:founding_year)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+puts "Clearing old data..."
+Company.destroy_all
+Dev.destroy_all
+
 puts "Creating companies..."
 Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
@@ -11,11 +15,17 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+10.times do
+    company_id = Company.ids.sample
+    dev_id = Dev.ids.sample
+    value = rand(10..100)
+    item_name = Faker::Name.name
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
 
+    Freebie.create(item_name: item_name, company_id: company_id, dev_id: dev_id, value: value)
+end
 puts "Seeding done!"
